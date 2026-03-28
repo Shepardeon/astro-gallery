94cf56b85c6de3d0c1f9e4a7d29d71b5dcf64b8d
Shepardeon <thomas.demaen@life-sensation.fr>
2026-03-28 13:08:09 +0100

    first commit

---
+++ ci/compose.ps1
@@ -0,0 +1,1 @@
+docker compose -p "astro-gallery" up -d
---
+++ gallery/astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [vue()]
+});
